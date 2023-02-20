fca26d51010376a566d9fce5d97726e8cbcaabeb
Gabriel <gabriel80546@gmail.com>
2023-02-20 19:13:20 -0300

    asdfasdf

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.getElementById("quadrado1").addEventListener("click", function() {
+    window.location.href = "jogo1.html";
+});
+document.getElementById("quadrado2").addEventListener("click", function() {
+    window.location.href = "jogo2.html";
+});
+document.getElementById("quadrado3").addEventListener("click", function() {
+    window.location.href = "jogo3.html";
+});
